e35910b9c817750322e6eed44b7011e9ca3f48b3
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2023-12-09 11:54:01 +0100

    fix(bump): Inline clipanion to fix yarn install (#10)

--- packages/bump/tsup.config.ts
+++ packages/bump/tsup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
+export default defineConfig(({ watch }) => ({
   entry: ['./src/index.ts', './src/bin.ts'],
   format: ['cjs', 'esm'],
   outDir: './out',
   dts: true,
+  clean: !watch,
+  minify: !watch,
   outExtension({ format }) {
     return { js: format === 'cjs' ? '.cjs' : '.mjs' };
   },
-});
+}));
